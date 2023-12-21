6c65e8c585345ffefa41b5aa017e5edd117da226
ex106 <ex106@RP21SYS11.win2k18.local>
2023-12-21 09:58:17 +0530

    aaa

--- aaa/src/main/java/aaa/aaa/add.java
+++ aaa/src/main/java/aaa/aaa/add.java
@@ -1,5 +1,10 @@
 package aaa.aaa;
 
 public class add {
-
+	public static void main(String[] args){
+		int a = 10;
+		int b = 20;
+		int sum = a+b;
+		System.out.println("Addition= "+sum);
+	}
 }
--- aaa/src/main/java/aaa/aaa/sub.java
+++ aaa/src/main/java/aaa/aaa/sub.java
@@ -1,5 +1,10 @@
 package aaa.aaa;
 
 public class sub {
-
+	public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int sub = a-b;
+        System.out.println("Addition= "+sub);
+}
 }
